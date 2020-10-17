1b1a336a1e7dd110ca48ca530d1edb0f1b16eb49
AmroWaleed <amro.alkharoof@gmail.com>
2020-10-17 22:36:16 +0300

    second commit

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+const { Router } = require('express');
 const express = require('express');
+const router =require('./routes');
+require("dotenv").config();
 const app = express();
 app.use(express.json());
+app.use(Router); 
+
+
 let dataBase = [
-    {id:1,actorName:"Pablo",paintings:"a",Parice:00},
-    {id:2,actorName:"Davenci",paintings:"b",Parice:00},
-    {id:3,actorName:"Van Gogh",paintings:"c",Parice:00},
-    {id:4,actorName:"Michel Angelo",paintings:"d",Parice:00},
-    {id:5,actorName:"Edvard Munch",paintings:"e",Parice:00}
+    {id:1,artistName:"Pablo",paintings:"a",price:00},
+    {id:2,artistName:"Davenci",paintings:"b",price:00},
+    {id:3,artistName:"Van Gogh",paintings:"c",price:00},
+    {id:4,artistName:"Michel Angelo",paintings:"d",price:00},
+    {id:5,artistName:"Edvard Munch",paintings:"e",price:00}
 ];
 
 app.get('/id',(req,res)=>{
@@ -15,15 +21,25 @@ app.get('/id',(req,res)=>{
     res.json(dataBase)
 })
 
+app.get('/artistName',(req,res)=>{
+    console.log("take a look on our artists")
+    res.json(dataBase)
+})
+
+app.get('/paintings',(req,res)=>{
+    console.log("there are a most famous paintings")
+    res.json(dataBase)
+})
+
 app.put('/paintings/:index',(req,res)=>{
     const {index}=req.params
-    const {newPaintings}=req.body
+    const {newPaintings}=req.body // new put
     dataBase[index].paintings=newPaintings
     res.json(dataBase)
 })
 
 app.post('/actorName',(req,res)=>{
-    dataBase.push(req,body)
+    dataBase.push(req.body)
     
     res.json(dataBase)
 })
@@ -37,7 +53,7 @@ app.delete('/actorName',(req,res)=>{
 
 // i have an error but i don`t know how to handle it -_-
 app.delete('/id/paintings/:paintings',(req,res) =>{       
-    dataBase.filter((element,index)=>{
+    dataBase= dataBase.filter((element,index)=>{
         return element.paintings != req.params.paintings
     });
     res.json(dataBase)
@@ -46,7 +62,7 @@ app.delete('/id/paintings/:paintings',(req,res) =>{
 
 /* 
 */
-const port = 3000;
+const port = 3000 || process.env.port;
 app.listen(port, () => {
   console.log(`SERVER ON at http://localhost:${port}`);
 });
